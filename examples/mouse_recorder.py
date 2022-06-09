790e474c27293ffb36fcae7fdf6740a7790a8a6b
Manu <mcabral@frba.utn.edu.ar>
2022-06-08 21:07:37 -0300

    added a mouse recorder example

---
+++ examples/mouse_recorder.py
@@ -0,0 +1,31 @@
+from cornelius import Cursor, MouseEvent
+from time import time, sleep
+
+"""
+    A simple mouse recorder that records mouse movement.
+    Note: This is a very simple implementation.
+"""
+
+mouse = Cursor()
+positions = []
+
+velocity = 0.1 # in seconds
+record_time = 5 # in seconds
+
+start = time()
+elapsed = 0
+print("Recording..")
+while elapsed < record_time:
+    position = mouse.get_position()
+    positions.append(position)
+    elapsed = time() - start
+    sleep(velocity)
+
+print('Recorded successfully!')
+print('Executing recorded sequence')
+
+for position in positions:
+    x, y = position
+    mouse.move(x=x, y=y)
+    sleep(velocity)
+
